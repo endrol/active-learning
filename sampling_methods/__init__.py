00b68c263fc7fbdf0b17a08136d0de970586628f
endrol <luodamingsisu@gmail.com>
2021-11-02 15:31:25 +0900

    make easier for use

--- sampling_methods/__init__.py
+++ sampling_methods/__init__.py
@@ -12,3 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .bandit_discrete import BanditDiscreteSampler
+from .graph_density import GraphDensitySampler
+from .hierarchical_clustering_AL import HierarchicalClusterAL
+from .informative_diverse import InformativeClusterDiverseSampler
+from .kcenter_greedy import kCenterGreedy
+from .margin_AL import MarginAL
+from .mixture_of_samplers import MixtureOfSamplers
+from .represent_cluster_centers import RepresentativeClusterMeanSampling
+from .simulate_batch import SimulateBatchSampler
+from .uniform_sampling import UniformSampling
+
+__all__ = [
+    'BanditDiscreteSampler',
+    'GraphDensitySampler',
+    'HierarchicalClusterAL',
+    'InformativeClusterDiverseSampler',
+    'kCenterGreedy',
+    'MarginAL',
+    'MixtureOfSamplers',
+    'RepresentativeClusterMeanSampling',
+    'SimulateBatchSampler',
+    'UniformSampling'
+]
